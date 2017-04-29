385fd61c24cf38bc7539a86cd5ca644840ca3a04
None <None>
2017-04-28 20:10:24 -0400

    The first check in.

---
+++ Ballmer's Peak/Lists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallmersPeak
+{
+    public class Lists
+    {
+
+        List<Stuff> TheList = new List<Stuff>();
+
+
+
+    }
+
+    public class Stuff
+        {
+
+        public string Name { get; private set; }
+
+
+
+        }
+
+}
